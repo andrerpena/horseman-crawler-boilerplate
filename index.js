5957858c3245a6d3607c9b372dbf06d88261e2eb
André Pena <andrerpena@gmail.com>
2016-09-16 17:39:18 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+#! /usr/bin/env node
+
+var Horseman = require('node-horseman');
+var horseman = new Horseman({loadImages:false, timeout: 60000});
+var spinner = require('@exponent/simple-spinner');
+var download = require('download');
+var fs = require('fs');
+require("colors");
+
+var date = process.argv[2];
+if(!date) throw Error('You did not specify the date');
+
+var pdf;
+
+horseman
+  .userAgent('Mozilla/5.0 (Windows NT 6.1; WOW64; rv:27.0) Gecko/20100101 Firefox/27.0')
+  .log("Consultando TJPA na data ".green + date.blue + "...".green )
+  .then(() => spinner.start())
+  .post('http://dje.tjpa.jus.br/dje_consulta/consultarDiario.do', "data=15/09/2016")
+  .then(() => spinner.stop())
+  .evaluate(function() { return $($("#pageBox1 p a")[1]).attr("href") })
+  .then(x => {
+     pdf = "http://dje.tjpa.jus.br" + x;
+     console.log("Achei o documento para donwload: ".green + pdf.blue);
+   })
+  .log("Downloading...".green)
+  .then(() => spinner.start())
+  .then(() => download(pdf))
+  .then((data) => fs.writeFileSync(date.replace(/\//g, "") + '.pdf', data))
+  .then(() => spinner.stop())
+  .then(() => process.exit());
